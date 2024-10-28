38e29dec468682590a02125cb0ef7ccf033754e1
EmanuelRodriguezBedeman <emanuel.rodriguez.bedeman@gmail.com>
2024-10-28 19:08:11 -0300

    Subida solucion clase 3 (landing.sh)

---
+++ Clase 3/landing.sh
@@ -0,0 +1,14 @@
+# Indica que interprete debe usar el script
+#!/bin/bash
+
+# Genera permisos para ejecutar el script
+su hadoop
+
+# Descarga el archivo a la carpeta landing
+wget -P /home/hadoop/landing https://github.com/fpineyro/homework-0/blob/master/starwars.csv
+
+# Mueve el archivo a HDFS Ingest
+hdfs dfs -put /home/hadoop/landing/starwars.csv /ingest
+
+# Borra el archivo del Landing
+rm /home/hadoop/landing/starwars.csv
